7769dfe0be63b400a1188b47e72f7f4d4b0c1118
too-much-magic <dylancrombie@outlook.com>
2024-12-03 17:11:18 -0700

    Reformatted files

--- day3p2.py
+++ day3p2.py
@@ -1,29 +1,32 @@
+def verify_mul(segment, i):
+    if segment[i:i + 4] == "mul(":
+        head = i + 4
+        mode = 1
+        num1, num2 = "", ""
+        while head < len(segment):
+            if segment[head] not in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','):
+                break
+            elif segment[head] == ',':
+                mode = 2
+            elif mode == 1:
+                num1 += segment[head]
+            elif mode == 2:
+                num2 += segment[head]
+            head += 1
+        if head < len(segment) - 1 and segment[head] != ')':
+            return 0
+        else:
+            if num1 != "" and num2 != "":
+                return int(num1) * int(num2)
+    return 0
+
 with open("day3.txt", 'r') as file:
     mult_sum = 0
     do = True
     for line in file:
         for n in range(len(line)):
             if do:
-                if line[n:n+4] == "mul(":
-                    head = n+4
-                    mode = 1
-                    num1,num2 = "",""
-                    while head < len(line):
-                        if line[head] not in ('0','1','2','3','4','5','6','7','8','9',','):
-                            break
-                        elif line[head] == ',':
-                            mode = 2
-                        elif mode == 1:
-                            num1 += line[head]
-                        elif mode == 2:
-                            num2 += line[head]
-                        head += 1
-                    if line[head] != ')':
-                        head += 1
-                    else:
-                        if num1 != "" and num2 != "":
-                            mult_sum += int(num1) * int(num2)
-                            print(num1,num2)
+                mult_sum += verify_mul(line, n)
             if line[n:n+4] == "do()":
                 do = True
             elif line[n:n+7] == "don't()":
